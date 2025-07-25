fb5e7709d38942e38ba627fbbb0ef30b7ef940a3
jirikora <jirikora530@gmail.com>
2025-07-25 22:11:48 +0700

    Refactor abit

--- server/src/storage/storageAdapter.ts
+++ server/src/storage/storageAdapter.ts
@@ -3,17 +3,17 @@ import memoryStorage from './memoryStorage'
 
 const twigStorage = memoryStorage<TwigTemplate>()
 
-export function storageAdapter() {
-  return {
-    twig() {
-      return {
-        get(key: string) {
-          return twigStorage.get(key)
-        },
-        set(key: string, value: TwigTemplate) {
-          return twigStorage.set(key, value)
-        },
-      }
+const singletonAdapter = {
+  twig: () => ({
+    get(key: string) {
+      return twigStorage.get(key)
     },
-  }
+    set(key: string, value: TwigTemplate) {
+      return twigStorage.set(key, value)
+    },
+  }),
+}
+
+export function storageAdapter() {
+  return singletonAdapter
 }
